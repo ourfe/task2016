90b970644cd3b36641a95e2b2c636d63cb92faa9
jingwang88 <wj154419@163.com>
2016-03-18 17:29:55 +0800

    兼容IE

--- js/index.js
+++ js/index.js
@@ -1,19 +1,53 @@
 window.onload = function() {
 	var phase = document.getElementById("phase1");
-	phase.addEventListener('mouseover', function(e) {
-		if (e.target.className == "task") {
-			console.log("mouseover");
-			e.target.style.backgroundColor = "rgb(32, 64, 86)";
-		}else if (e.target.parentNode.className == "task") {
-			e.target.parentNode.style.backgroundColor = "rgb(32, 64, 86)";
+	// 跨浏览器处理函数
+	var EventUtil = {
+		addHandler: function(element, type, handler) {
+			if (element.addEventListener) {
+				element.addEventListener(type, handler, false);
+			}else if (element.attachEvent) {
+				element.attachEvent("on" + type, handler);
+			}else {
+				element["on" + type] = handler;
+			}
+		},
+		removeHandler: function(element, type, handler) {
+			if (element.removeEventListener) {
+				element.removeEventListener(type, handler);
+			}else if (element.detachEvent) {
+				element.detachEvent("on" + type, handler);
+			}else {
+				element["on" + type] = null;
+			}
 		}
-	},true);
-	phase.addEventListener('mouseout', function(e) {
-		if (e.target.className == "task") {
+	};
+	// 兼容IE
+	function getSrcElement(event) {
+		if (event.target) {
+			return event.target;
+		}else {
+			return event.srcElement;
+		}
+	}
+
+	EventUtil.addHandler(phase, 'mouseover', function(e) {
+		var target = getSrcElement(e);
+		if (target.className == "task") {
+			console.log("mouseover");
+			target.style.backgroundColor = "rgb(32, 64, 86)";
+		}else if (target.parentNode.className == "task") {
+			console.log("mouseover parent");
+			target.parentNode.style.backgroundColor = "rgb(32, 64, 86)";
+		}
+	});
+	EventUtil.addHandler(phase, 'mouseout', function(e) {
+		var target = getSrcElement(e);
+		if (target.className == "task") {
 			console.log("mouseout");
-			e.target.style.backgroundColor = "white";
-		}else if (e.target.parentNode.className == "task") {
-			e.target.parentNode.style.backgroundColor = "white";
+			target.style.backgroundColor = "white";
+		}else if (target.parentNode.className == "task") {
+			console.log("mouseout");
+			target.parentNode.style.backgroundColor = "white";
 		}
 	});
 }
